f5d82e02a42c1e79a908f37facc069c255e27120
Scott Chamberlain <sckott@fastmail.com>
2021-07-08 15:22:01 -0700

    go back to full script

--- .github/workflows/check_package.rb
+++ .github/workflows/check_package.rb
@@ -6,18 +6,15 @@ require 'json'
 iss_no = ENV["ISSUE_NUMBER"]
 iss_body = ENV["ISSUE_BODY"]
 
-puts iss_no
-puts iss_body
+repo_url = iss_body.split(/\n/).select {|z| z.match(/Repository/)}.first[/https:.*/].strip
 
-# repo_url = iss_body.split(/\n/).select {|z| z.match(/Repository/)}.first[/https:.*/].strip
+base = "http://138.68.123.59:8000"
+params = {repourl: repo_url, repo: "sckott/studious-computing-machine", issue_id: iss_no}
+conn = Faraday.new base
+res = conn.post('/editorcheck', params)
 
-# base = "http://138.68.123.59:8000"
-# params = {repourl: repo_url, repo: "sckott/studious-computing-machine", issue_id: iss_no}
-# conn = Faraday.new base
-# res = conn.post('/editorcheck', params)
-
-# if not res.success?
-#   raise "Editor check failed, see logs"
-# else
-#   puts JSON.parse(res.body).first
-# end
+if not res.success?
+  raise "Editor check failed, see logs"
+else
+  puts JSON.parse(res.body).first
+end
