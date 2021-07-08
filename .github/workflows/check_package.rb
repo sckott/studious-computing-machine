da52aa4472c84b5593b01702dbefdd1f3af4ac94
Scott Chamberlain <sckott@fastmail.com>
2021-07-08 15:19:03 -0700

    just puts env vars to make sure they are working

--- .github/workflows/check_package.rb
+++ .github/workflows/check_package.rb
@@ -6,15 +6,18 @@ require 'json'
 iss_no = ENV["ISSUE_NUMBER"]
 iss_body = ENV["ISSUE_BODY"]
 
-repo_url = iss_body.split(/\n/).select {|z| z.match(/Repository/)}.first[/https:.*/].strip
+puts iss_no
+puts iss_body
 
-base = "http://138.68.123.59:8000"
-params = {repourl: repo_url, repo: "sckott/studious-computing-machine", issue_id: iss_no}
-conn = Faraday.new base
-res = conn.post('/editorcheck', params)
+# repo_url = iss_body.split(/\n/).select {|z| z.match(/Repository/)}.first[/https:.*/].strip
 
-if not res.success?
-  raise "Editor check failed, see logs"
-else
-  puts JSON.parse(res.body).first
-end
+# base = "http://138.68.123.59:8000"
+# params = {repourl: repo_url, repo: "sckott/studious-computing-machine", issue_id: iss_no}
+# conn = Faraday.new base
+# res = conn.post('/editorcheck', params)
+
+# if not res.success?
+#   raise "Editor check failed, see logs"
+# else
+#   puts JSON.parse(res.body).first
+# end
