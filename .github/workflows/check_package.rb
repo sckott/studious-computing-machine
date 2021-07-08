4fafac4c2cd884fc9de6d8790d8b6d05455daaf6
Scott Chamberlain <sckott@fastmail.com>
2021-07-08 15:09:37 -0700

    add ruby script and gemfiles

---
+++ .github/workflows/check_package.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'faraday'
+require 'json'
+
+iss_no = ENV["ISSUE_NUMBER"]
+iss_body = ENV["ISSUE_BODY"]
+
+repo_url = iss_body.split(/\n/).select {|z| z.match(/Repository/)}.first[/https:.*/].strip
+
+base = "http://138.68.123.59:8000"
+params = {repourl: repo_url, repo: "sckott/studious-computing-machine", issue_id: iss_no}
+conn = Faraday.new base
+res = conn.post('/editorcheck', params)
+
+if not res.success?
+  raise "Editor check failed, see logs"
+else
+  puts JSON.parse(res.body).first
+end
